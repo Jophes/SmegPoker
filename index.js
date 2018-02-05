c5ca4018e347287419835cfa5ec3b36932fb87b3
Joseph Higgins <jopheshiggins@gmail.com>
2018-02-05 14:02:08 +0000

    updates

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+const port = process.env.PORT || 8888;
 
-// 8888
+function GetTime() {
+    var time = new Date();
+    var secs = (time.getSeconds() + time.getMilliseconds() * 0.001).toFixed(3)
+    var seconds = (secs.length < 6 ? '0' : '') + secs;
+    var minutes = (time.getMinutes().toString().length < 2 ? '0' : '') + time.getMinutes();
+    var hours = (time.getHours().toString().length < 2 ? '0' : '') + time.getHours();
+    return '' + hours + ':' + minutes + ':' + seconds + ' -';
+}
+
+// Listen for connections
+function HandleServerStartup() {
+    console.log(GetTime() + ' [SERVER] Server listening on port %d', port);
+}
+server.listen(port, HandleServerStartup);
+
+
+// -- Express routing --
+function AppLog(msg, req) {
+    console.log(GetTime() + ' [APP] ' + msg + ' {' + req.ip + ':' + req.client.remotePort + '}');
+}
+
+function LogRequests(req, res, next) {
+    AppLog('Recieved ' + req.method + ' request for "' + req.url + '" from', req);
+    next();
+}
+
+app.use(LogRequests);
+app.use(express.static(__dirname + '/Public'));
+
